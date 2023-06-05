6389bd7e35f834b9cff7c9fb3c6ba3353253ca86
aaron <redskin.rox@gmail.com>
2023-06-05 15:48:40 -0400

    added all commands
    theres are still bugs with the commands that im fixing

--- music_bot/main.py
+++ music_bot/main.py
@@ -2,10 +2,23 @@ import discord
 from discord.ext import commands
 import os
 
+#import all of the cogs
+from help_cog import help_cog
 from music_cog import music_cog
 
-bot = commands.Bot(command_prefix='#')
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+
+#remove the default help command so that we can write out own
+bot.remove_command('help')
+
+#register the class with the bot
 bot.add_cog(help_cog(bot))
 bot.add_cog(music_cog(bot))
 
-bot.run(os.getenv("TOKEN"))
+#start the bot with our token
+TOKEN = "put your discord bot token here"
+bot.run(TOKEN)
